ecb92cd6fc4770b938c7cc8d264049d7ebac4542
shuhey <shuhey.dev@gmail.com>
2019-07-18 23:01:54 +0900

    add edit feature

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -48,7 +48,12 @@ namespace EmployeeManagement
             }
 
             app.UseStaticFiles();
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
