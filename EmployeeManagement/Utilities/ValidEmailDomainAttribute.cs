c414584617b5c8d79f53fafddce9e606245a483f
shuhey <shuhey.dev@gmail.com>
2019-07-20 11:42:31 +0900

    add custom validation

---
+++ EmployeeManagement/Utilities/ValidEmailDomainAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Utilities
+{
+    public class ValidEmailDomainAttribute : ValidationAttribute
+    {
+        private readonly string _allowedDomain;
+
+        public ValidEmailDomainAttribute(string allowedDomain)
+        {
+            _allowedDomain = allowedDomain;
+        }
+        public override bool IsValid(object value)//入力欄に入力された内容がvalueに入ってくる
+        {
+            string[] strings = value.ToString().Split('@');
+            return strings[1].ToUpper() == _allowedDomain.ToUpper();
+        }
+    }
+}
